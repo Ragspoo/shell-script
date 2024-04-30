75a2e774930c15b0559cc0a0f7662ea2a39972f2
raghu <sonurag11@gmail.com>
2024-04-30 19:16:32 +0530

    added functions

---
+++ 10. Functions.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+Id=$(id -u)
+
+Validate(){
+    if [ $1 -ne 0 ]
+    then
+        echo "ERR: $2... Failed"
+        exit 1
+    else
+        echo "$2... Success"
+    fi
+}
+
+if [ $Id -ne 0 ]{
+then
+    echo "ERR: Please run with root access"
+    exit 1
+else
+    echo "You are root user"
+}
+
+yum install mysql -y
+
+Validate $? "installing mysql"
+
+yum install git -y
+
+Validate $? "installing git"
