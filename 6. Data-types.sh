664aabfd23fce3b0c041340d00e4a20e3fbf7818
raghu <sonurag11@gmail.com>
2024-10-20 20:52:41 +0530

    syntax change

--- 6. Data-types.sh
+++ 6. Data-types.sh
@@ -9,5 +9,5 @@ echo "Addition of two numbers: $Sum"
 
 #Special characters in shellscript
 echo "How many args passed: $#" #to print how many arguments are passed we use $#
-echo "All args passed: $@" # $@-> to print all the arguments
+echo "which args passed: $@" # $@-> to print all the arguments
 echo "script name: $0" # #0-> to print the script name
